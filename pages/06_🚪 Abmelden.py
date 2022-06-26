7a75be7a38cbae7d25ebda8ce05ad34ee3b43bbb
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-26 17:36:51 +0200

    Update 06_🚪 Abmelden.py

--- pages/06_🚪 Abmelden.py
+++ pages/06_🚪 Abmelden.py
@@ -23,7 +23,7 @@ def load_lottieurl2(url: str):
   
     
 def app():
-    st.write(st.session_state.name /n
+    st.write(st.session_state.name
             "Möchten Sie sich wirklich ausloggen?")
     with st.form("button"):
         ja=st.form_submit_button(label="Ja")

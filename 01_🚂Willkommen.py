dbb493df6b498608e0765cd85819441da3da0a1c
KatjaGr <106029275+KatjaGr@users.noreply.github.com>
2022-06-26 16:33:30 +0200

    Update 01_🚂Willkommen.py

--- 01_🚂Willkommen.py
+++ 01_🚂Willkommen.py
@@ -364,14 +364,14 @@ def app():
                                     
                                       else:
                                         st.warning("Der Name dieser Anfrage existiert bereits. Bitte wähle einen Anderen.")
-                                   
-                                    mehrereanfragen(loginn,wunsch)
+                    
+                                  mehrereanfragen(loginn,wunsch)
 #weiter2=st.form_submit_button("Fortfahren zum Diagramm/Preisvorhersage")
                                       
-                                    if 'name' not in st.session_state:
-                                          st.session_state.name =loginn
-                                    if 'passw' not in st.session_state:
-                                          st.session_state.passw=loginp
+                              if 'name' not in st.session_state:
+                                    st.session_state.name =loginn
+                              if 'passw' not in st.session_state:
+                                    st.session_state.passw=loginp
                                   
 
                                                                      
@@ -379,10 +379,10 @@ def app():
 
 
                   
-                    if best:
-                      Login(loginn,loginp)
-                      if 'willen' not in st.session_state:
-                        st.session_state.willen= True
+                          if best:
+                            Login(loginn,loginp)
+                            if 'willen' not in st.session_state:
+                                st.session_state.willen= True
                           
                   if option=="Registrieren":
                       with st.form(key='form201'):

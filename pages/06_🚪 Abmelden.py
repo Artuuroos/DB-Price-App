358f387506f1164f940aafb346ee12f100c6c2cc
KatjaGr <106029275+KatjaGr@users.noreply.github.com>
2022-06-26 16:25:47 +0200

    Update 06_🚪 Abmelden.py

--- pages/06_🚪 Abmelden.py
+++ pages/06_🚪 Abmelden.py
@@ -17,7 +17,7 @@ def load_lottieurl2(url: str):
   if f.status_code != 200:
      return None
   return f.json()
-  lottie_url_hello2 = "https://assets10.lottiefiles.com/packages/lf20_dfv2eehv.json"
+  lottie_url_hello2 = "https://assets10.lottiefiles.com/packages/lf20_bhxvhkdy.json"
  #"https://assets3.lottiefiles.com/packages/lf20_E3exCx.json"
   
   
@@ -34,13 +34,13 @@ def app():
         
     if nein:
         st.info("Nicht abgemeldet")
-    lottie_url_hello2 = "https://assets4.lottiefiles.com/packages/lf20_7mibdcvp.json"
+    lottie_url_hello2 = "https://assets10.lottiefiles.com/packages/lf20_bhxvhkdy.json"
     lottie_hello2 = load_lottieurl2(lottie_url_hello2)
     st_lottie(lottie_hello2, key="hello") 
 app()
 
 
-load_lottieurl2("https://assets10.lottiefiles.com/packages/lf20_dfv2eehv.json")
+load_lottieurl2("https://assets10.lottiefiles.com/packages/lf20_bhxvhkdy.json")
  
 #def load_lottieurl2(url: str):
  #   f = requests.get(url)

adf228cdc7516991745df52dd7314c84b0dbe01e
Artuuroos <106031033+Artuuroos@users.noreply.github.com>
2022-06-26 20:28:39 +0200

    Update 03_📈 Diagramm & Preisprognose.py

--- pages/03_📈 Diagramm & Preisprognose.py
+++ pages/03_📈 Diagramm & Preisprognose.py
@@ -21,9 +21,8 @@ cursor = conn.cursor()
 def app():
   coll1,coll2,coll3,coll4=st.columns(4)
   st.subheader("Sind Sie bereits eingeloggt?")
-  with st.form("button1"):
-        jabox=st.form_submit_button(label="Ja")
-        neinbox=st.form_submit_button(label="Nein")
+  jabox=st.checkbox(label="Ja")
+  neinbox=st.checkbox(label="Nein")
   if neinbox:            
       loginname=st.text_input("Login:")
       loginpassw=st.text_input("Passwort:",type="password")
